41ab7bc29ade18c9e21de4f7ed1573f172fb59ea
tracyann <tracyann@tracyann-pc.northstate.net>
2020-10-14 21:10:21 -0400

    Working on frontend

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,14 @@ import React from 'react';
 function App() {
 
   return(
-    <div>
-      <h1>SellEasy</h1>
+    <div className="App">
+      <div className="jumbotron">
+        <h1>SellEasy</h1>
+        <h3>dedicated to helping you sell</h3>
+      </div>
+      <div className="container">
+        <p>Sales in your area</p>
+      </div>
     </div>
   )
 }
