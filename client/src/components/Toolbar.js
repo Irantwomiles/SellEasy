701212e9a1aedf04e15675f44a20a7c65a7fb357
Shayan <irantwomiles@gmail.com>
2020-11-07 19:07:50 -0500

    p

---
+++ client/src/components/Toolbar.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react';
+
+function Toolbar() {
+    const [color, setColor] = setState();
+
+    
+    return (
+        <div>
+            <div>
+                <p>{color}</p>
+            </div>
+        </div>
+
+    )
+}
