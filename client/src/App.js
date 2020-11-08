6798439bbc25d9d74d060639fe3a77dba9f661ee
tracyann <tracyann@tracyann-pc.northstate.net>
2020-11-07 20:19:06 -0500

    Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import './App.css';
+import Toolbar from './components/Toolbar.js';
 
 function App() {
 
   return(
     <div className="App">
       <div className="jumbotron">
-        <h1>SellEasy</h1>
-        <h3>dedicated to helping you sell</h3>
+        <div class="topline">
+          <div class="welcome">
+            <h1>SellEasy</h1>
+            <h3>dedicated to helping you sell</h3>
+          </div>
+          <Toolbar></Toolbar>
+        </div>
       </div>
       <div className="container">
         <p>Sales in your area</p>
