8de6bfdc14e3d39bfbb67d7ca4b366a640e1b9e0
Shayan <irantwomiles@gmail.com>
2020-11-12 14:48:46 -0500

    new push

--- client/src/components/ViewallGarageSales.js
+++ client/src/components/ViewallGarageSales.js
@@ -1,6 +1,5 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
-import GarageSale from './GarageSale';
 import { Container, Row, Card } from 'react-bootstrap';
 
 function ViewallGarageSales(){
