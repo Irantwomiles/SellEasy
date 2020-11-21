8e4fe86bf00226bb670e1ac48744637a55931759
tracyann <tracyann@tracyann-pc.northstate.net>
2020-11-21 18:14:08 -0500

    ViewPost

---
+++ client/src/components/ViewPost.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { Form, Button, Col, Row, Container} from 'react-bootstrap';
+import Cookies from 'universal-cookie';
+import axios from 'axios';
+
+function ViewPost() {
+
+    return (
+        <Container>
+            
+        </Container>
+
+
+
+
+
+    )
+}
