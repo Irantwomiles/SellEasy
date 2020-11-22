aeee344f1f56aa0d24f4cd6fc220bd164c724781
Shayan <irantwomiles@gmail.com>
2020-11-21 19:59:18 -0500

    test

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const postRouter = require('./routes/post');
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(bodyParser.json());
