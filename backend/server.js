27dc425f2cada08309f270274d1eee786dfed50a
Shayan <irantwomiles@gmail.com>
2020-11-21 19:17:45 -0500

    scripts

--- backend/server.js
+++ backend/server.js
@@ -14,9 +14,15 @@ const postRouter = require('./routes/post');
 
 const app = express();
 
+const PORT = process.env.PORT || 5000;
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('../client/build'));
+}
 
 app.use('/api', authRouter);
 app.use('/api', userRouter);
@@ -30,4 +36,4 @@ app.get('/api', function(req, res) {
     res.send("Nothing here, move along!");
 })
 
-app.listen(5000, () => console.log("Server started on port 5000"));
+app.listen(PORT, () => console.log("Server started on port 5000"));
