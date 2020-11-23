24779acbb1bfb5d51e497054ab8e0e34dae852cc
tracyann <tracyann@tracyann-pc.northstate.net>
2020-11-23 14:22:01 -0500

    image

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -4,12 +4,18 @@ import { Container, Card, Table, Button, InputGroup } from 'react-bootstrap';
 import axios from 'axios';
 import Cookies from 'universal-cookie';
 import "../App.css";
+import logo from '../shoppingbags.png';
 
 
 function About() {
 
     return (
-        <h1>Sell Easy</h1>
+        <Container> 
+            <h1 className="title">SellEasy</h1>
+            <h2 className="desc">Create listings, view items, and many more! Sign up for a free account today!</h2>
+            <img className="pic" src={logo} alt="Logo" />
+        </Container>
+        
     )
     
 
